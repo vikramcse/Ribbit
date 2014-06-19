c90382e38a76595f8ca23ac52d076365ce5c57bd
vikram Jadhv <vikramcse.10@gmail.com>
2014-06-19 16:21:29 +0530

    Sending Messages to recipients

--- app/src/main/java/com/vikram/ribbit/app/MainActivity.java
+++ app/src/main/java/com/vikram/ribbit/app/MainActivity.java
@@ -262,6 +262,19 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                 sendBroadcast(mediaScanIntent);
             }
 
+            Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+            recipientsIntent.setData(mMediaUri);
+
+            String fileType = "";
+            if (requestCode == PICK_PHOTO_REQUEST || requestCode == TAKE_PHOTO_REQUEST) {
+                fileType = ParseConstants.TYPE_IMAGE;
+            }
+            else if (requestCode == PICK_VIDEO_REQUEST || requestCode == TAKE_VIDEO_REQUEST) {
+                fileType = ParseConstants.TYPE_VIDEO;
+            }
+
+            recipientsIntent.putExtra(ParseConstants.KEY_FILE_TYPE, fileType);
+            startActivity(recipientsIntent);
         }
         else if (requestCode != RESULT_CANCELED){
             Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
--- app/src/main/java/com/vikram/ribbit/app/ParseConstants.java
+++ app/src/main/java/com/vikram/ribbit/app/ParseConstants.java
@@ -3,6 +3,19 @@ package com.vikram.ribbit.app;
 
 public final class ParseConstants {
 
+    // Class Name
+    public static final String CLASS_MESSAGES = "Messages";
+
+    // Field Names
     public static final String KEY_USERNAME = "username";
     public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+    public static final String KEY_RECIPIENT_IDS = "recipientsIds";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_FILE_TYPE = "fileType";
+
+    public static final String TYPE_IMAGE = "image";
+    public static final String TYPE_VIDEO = "video";
+
 }
